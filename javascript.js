83ad428d5d70304fd086b4c686d61543e9741de4
sykul <luke.flatley@outlook.com>
2023-09-12 22:07:19 +0200

    refactor rock paper scissors to use buttons as input

--- javascript.js
+++ javascript.js
@@ -16,57 +16,25 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    // prompt "Rock, paper, or scissors?"
-    // input to lowercase, save to variable
-    let input = prompt("Rock, paper or scissors?").toLowerCase();
-
-    // check if "rock", "paper" or "scissors" have been inputted, if not then run again
-    if (!(input == "rock" || input == "paper" || input == "scissors")) {
-        console.log("I'm sorry, I don't understand. Please type 'rock', 'paper', or 'scissors'");
-        return getPlayerChoice();
-    } else {
-        return input;
-    }
-}
-
-function playRound() {
-    const playerSelection = getPlayerChoice();
+function playRound(playerSelection) {
     const computerSelection = getComputerChoice();
-
     if (playerSelection == computerSelection) {
-        console.log("Draw. Choose again.");
-        return playRound();
+        return playRound(playerSelection);
     } else if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
         console.log("You lose, computer wins.");
         return "computerWin";
     } else if (computerSelection == "rock" && playerSelection == "paper" || computerSelection == "paper" && playerSelection == "scissors" || computerSelection == "scissors" && playerSelection == "rock") {
         console.log("You win, computer loses.");
         return "playerWin"
-        } else {
+    } else {
         console.log("Something has gone wrong.");
     }
 }
 
-function game() {
-    let playerWinCounter = 0;
-    let computerWinCounter = 0;
+const rockbutton = document.getElementById('rock');
+const paperbutton = document.getElementById('paper');
+const scissorsbutton = document.getElementById('scissors');
 
-    for (i=0; i<5; i++) {
-        result = playRound();
-        switch (result) {
-            case "playerWin":
-                playerWinCounter += 1;
-                break;
-            case "computerWin":
-                computerWinCounter += 1;
-                break;
-        }
-    }
-    
-    if (playerWinCounter > computerWinCounter) {
-        console.log("You're the champion!");
-    } else if ( computerWinCounter > playerWinCounter) {
-        console.log("Sorry, the computer has defeated you.")
-    }
-}
+rockbutton.addEventListener('click', () => playRound("rock"));
+paperbutton.addEventListener('click', () => playRound("paper"));
+scissorsbutton.addEventListener('click', () => playRound("scissors"));
